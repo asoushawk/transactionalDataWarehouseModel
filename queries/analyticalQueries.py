c69b7f4941962ae4a57ebe2c4dc64cccb44341ec
Daniel Soares <67944213+asoushawk@users.noreply.github.com>
2022-02-20 16:13:27 -0300

    refactor: code refactor

--- queries/analyticalQueries.py
+++ queries/analyticalQueries.py
@@ -8,9 +8,13 @@ cursor = conn.cursor()
 
 #Total bought by country
 def getTotalByCountry():
-	cursor.execute("""SELECT c.country, SUM(t.value_sold_brl) AS total_purchased FROM Sales_Fact as t
-	LEFT OUTER JOIN costumer_dimension AS c ON t.costumer_key = c.costumer_key
-	GROUP BY c.country ORDER BY SUM(t.value_sold_brl) DESC""")
+	cursor.execute("""SELECT 
+				c.country, 
+				SUM(t.value_sold_brl) AS total_purchased 
+			FROM Sales_Fact as t
+			LEFT OUTER JOIN costumer_dimension AS c ON t.costumer_key = c.costumer_key
+			GROUP BY c.country
+			ORDER BY SUM(t.value_sold_brl) DESC""")
 	
 	result = cursor.fetchall()
 	return result
@@ -18,10 +22,12 @@ def getTotalByCountry():
 
 #Total sold by store
 def getTotalByStore():
-	cursor.execute("""SELECT s.store_name, SUM(t.value_sold_brl) AS total_purchased
-		FROM Sales_Fact as t
-		LEFT OUTER JOIN store_dimension AS s ON t.store_key = s.store_key
-		GROUP BY s.store_name""")
+	cursor.execute("""SELECT
+				s.store_name, 
+				SUM(t.value_sold_brl) AS total_purchased
+			FROM Sales_Fact as t
+			LEFT OUTER JOIN store_dimension AS s ON t.store_key = s.store_key
+			GROUP BY s.store_name""")
 
 
 	result = cursor.fetchall()
@@ -31,9 +37,11 @@ def getTotalByStore():
 
 #Product with the most units sold
 def getProductWithMostUnitsSold():
-	cursor.execute("""SELECT p.product_title, SUM(t.units_sold) as total_sold FROM Sales_Fact as t
-		LEFT OUTER JOIN product_dimension AS p ON p.product_key = t.product_key
-		GROUP BY p.product_title ORDER BY SUM(t.units_sold) DESC""")
+	cursor.execute("""SELECT p.product_title, 
+				SUM(t.units_sold) as total_sold 
+			FROM Sales_Fact as t
+			LEFT OUTER JOIN product_dimension AS p ON p.product_key = t.product_key
+			GROUP BY p.product_title ORDER BY SUM(t.units_sold) DESC""")
 
 	result = cursor.fetchall()
 	return result
@@ -42,20 +50,26 @@ def getProductWithMostUnitsSold():
 
 #Day of the week with the most units sold
 def getBestDayOfTheWeek():
-	cursor.execute("""SELECT tm.dayoftheweek, SUM(t.units_sold), SUM(t.value_sold_brl) as best_day
-		FROM Sales_Fact as t
-		LEFT OUTER JOIN time_dimension AS tm ON tm.time_key = t.time_key
-		GROUP BY tm.dayoftheweek ORDER BY SUM(t.value_sold_brl) DESC""")
+	cursor.execute("""SELECT 
+				tm.dayoftheweek,
+				SUM(t.units_sold), 
+				SUM(t.value_sold_brl) as best_day
+			FROM Sales_Fact as t
+			LEFT OUTER JOIN time_dimension AS tm ON tm.time_key = t.time_key
+			GROUP BY tm.dayoftheweek ORDER BY SUM(t.value_sold_brl) DESC""")
 
 	result = cursor.fetchall()
 	return result
 
 #Best month based on total sold in brl
 def getBestMonth():
-	cursor.execute("""SELECT tm.month, SUM(t.units_sold), SUM(t.value_sold_brl) as best_day
-		FROM Sales_Fact as t
-		LEFT OUTER JOIN time_dimension AS tm ON tm.time_key = t.time_key
-		GROUP BY tm.month ORDER BY SUM(t.value_sold_brl) DESC""")
+	cursor.execute("""SELECT 
+				tm.month, 
+				SUM(t.units_sold), 
+				SUM(t.value_sold_brl) as best_day
+			FROM Sales_Fact as t
+			LEFT OUTER JOIN time_dimension AS tm ON tm.time_key = t.time_key
+			GROUP BY tm.month ORDER BY SUM(t.value_sold_brl) DESC""")
 
 	result = cursor.fetchall()
 	return result
@@ -64,10 +78,13 @@ def getBestMonth():
 
 
 def getClientProfile():
-	cursor.execute("""SELECT c.purchase_profile, SUM(t.value_sold_brl) AS pf
-		FROM Sales_Fact as t
-		LEFT OUTER JOIN costumer_dimension AS c ON c.costumer_key = t.costumer_key
-		GROUP BY c.purchase_profile ORDER BY SUM(t.value_sold_brl) DESC""")
+	cursor.execute("""SELECT 
+				c.purchase_profile,
+				SUM(t.value_sold_brl) AS pf
+			FROM Sales_Fact as t
+			LEFT OUTER JOIN costumer_dimension AS c ON c.costumer_key = t.costumer_key
+			GROUP BY c.purchase_profile
+			ORDER BY SUM(t.value_sold_brl) DESC""")
 
 	result = cursor.fetchall()
 	return result
